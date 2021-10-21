123a4971967ec9581276e343cb561ab8da12c1e8
m9rcy <m9rcy19@gmail.com>
2021-10-21 23:38:11 +1300

    🚀 Initial commit

---
+++ kafka-avro/src/test/java/io/m9rcy/avro/KafkaAvroApplicationTests.java
@@ -0,0 +1,13 @@
+package io.m9rcy.avro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaAvroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
